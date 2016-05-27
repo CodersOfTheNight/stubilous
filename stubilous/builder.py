334140c23108d8e60d2a723c9f81f11e76f33b44
Šarūnas Navickas <sarunas.navickas@adform.com>
2016-05-27 13:13:36 +0300

    Basic builder

---
+++ stubilous/builder.py
@@ -0,0 +1,24 @@
+from functools import partial
+
+
+def response(cls, method, url, body, status=200):
+    from stubilous.config import Route
+    route = Route(method=method, path=url, body=body, status=status)
+    cls.routes.append(route)
+    return cls
+
+
+class Builder(object):
+
+    def __init__(self):
+        self.host = None
+        self.port = None
+        self.routes = []
+
+    def server(self, host, port):
+        self.host = host
+        self.port = port
+        return self
+
+    def with(self, method, url):
+        return partial(response, cls=self, method=method, path=url)
