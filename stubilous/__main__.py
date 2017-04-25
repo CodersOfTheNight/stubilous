2189e84dbfa25450ec7ac2cba84baf69aef234a5
Šarūnas Navickas <sarunas.navickas@adform.com>
2017-04-25 17:10:03 +0300

    Use click instead of argparse

--- stubilous/__main__.py
+++ stubilous/__main__.py
@@ -1,15 +1,17 @@
 import yaml
-
-from argparse import ArgumentParser
+import click
 
 from stubilous.server import run
 from stubilous.config import Config
 
 
-if __name__ == "__main__":
-    parser = ArgumentParser()
-    parser.add_argument("--config", help="Service configuration file")
-    args = parser.parse_args()
-    with open(args.config, "r") as f:
+@click.command()
+@click.option("--config", help="Service configuration file")
+def main(config_file):
+    with open(config_file, "r") as f:
         config = Config.from_dict(yaml.load(f))
     run(config)
+
+
+if __name__ == "__main__":
+    main()
